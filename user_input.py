ab65106a38054da044489804994013bcde25f425
c-Kuria <briankuria1812@gmail.com>
2024-08-17 16:40:10 +0300

    week-1-submission

---
+++ user_input.py
@@ -0,0 +1,7 @@
+fname = input("First Name: ")
+sname = input("Sir Name: ")
+yrOfBirth = int(input("Year Born?: "))
+age = 2024 - yrOfBirth
+location = input("Location?: ")
+
+print(f"Hell0 {fname.capitalize()} {sname}, you are {age} years old and live in {location.capitalize()}")
